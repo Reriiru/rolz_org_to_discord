9d999645e669613be2723f0fb06dce4db4460045
ivelme <ivelme@yandex.ru>
2018-01-07 21:41:41 +0500

    Let's try this shit.

--- rolz_bot/settings.py
+++ rolz_bot/settings.py
@@ -1,19 +1,42 @@
 import os
+import codecs
+import ast
 
 ENV = 'prod'
+
+
+def parse_option(value, type):
+    parsers = {
+        bool: lambda x: bool(ast.literal_eval(x))
+    }
+    parser = parsers.get(type, type)
+    return parser(value)
+
+
+def get_secret(name, *args, **kwargs):
+    try:
+        with codecs.open('/run/secrets/{}'.format(name), encoding='utf-8') \
+                                                                as stream:
+            value = stream.read()
+            return parse_option(value.strip(), kwargs.get('type', str))
+    except IOError:
+        if args:
+            return args[0]
+        raise RuntimeError(u"Not found {} in Docker secrets".format(name))
+
 
 if ENV == 'dev':
     pass
 if ENV == 'prod':
-    CONSUMER_KEY = os.environ['CONSUMER_KEY']
-    CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-    ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-    ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+    CONSUMER_KEY = get_secret('twitter_consumer_key')
+    CONSUMER_SECRET = get_secret('twitter_consumer_secret')
+    ACCESS_TOKEN = get_secret('twitter_access_token')
+    ACCESS_TOKEN_SECRET = get_secret('twitter_access_token_secret')
 
-    IMGUR_CLIENT = os.environ['IMGUR_CLIENT']
-    IMGUR_SECRET = os.environ['IMGUR_SECRET']
-    BOT_TOKEN = os.environ['BOT_TOKEN']
-    MONGO_STRING = os.environ['MONGO_STRING']
+    IMGUR_CLIENT = get_secret('imgur_client')
+    IMGUR_SECRET = get_secret('imgur_secret')
+    BOT_TOKEN = get_secret('discord_bot_token')
+    MONGO_STRING = '//mongo:27017/bot_data'
 
 
 STARTUP = ['extensions.rolz', 'extensions.twitter', 'extensions.choose',
